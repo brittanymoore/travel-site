d4d24c0d0ba50a57d46e43646b67c04da7c448b8
Brittany Moore <moore.brittanyann@gmail.com>
2017-11-16 10:53:00 -0500

    Completed section 8.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,2 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch');
-
-gulp.task('default', function () {
-    console.log('Hooray - you created a Gulp task!');
-});
-
-gulp.task('html', function () {
-    console.log('Something useful here...');
-});
-
-gulp.task('styles', function () {
-    console.log('Something useful here... (css)');
-});
-
-gulp.task('watch', function () {
-
-    watch('./app/index.html', function () {
-        gulp.start('html');
-    });
-
-    watch('./app/assets/styles/**/*.css', function () {
-        gulp.start('styles');
-    });
-
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
